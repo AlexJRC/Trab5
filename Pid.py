1b87522d23414767630b12b980ef6d0ac47cfd14
Ubuntu <ubuntu@ip-172-31-95-127.ec2.internal>
2020-11-16 19:51:52 +0000

    p controller

--- Pid.py
+++ Pid.py
@@ -6,12 +6,35 @@ import tf
 import math
 
 mat = [2017004555, 2016001094, 20166001960, 28090]
+#kp1 = 1
+kp2 = 0.01
+kp3 = 1
+
+#ki1 = 1
+ki2 = 0.0001
+ki3 = 0.01
+
+#kd1 = 1
+kd2 = 0.01
+kd3 = 1
+
+
 odom = Odometry()
 scan = LaserScan()
 
 rospy.init_node('cmd_node')
 
 # Auxiliar functions ------------------------------------------------
+
+
+
+def getAngle(msg):
+    quaternion = msg.pose.pose.orientation
+    quat = [quaternion.x, quaternion.y, quaternion.z, quaternion.w]
+    euler = tf.transformations.euler_from_quaternion(quat)
+    yaw = euler[2]*180.0/math.pi
+    return yaw
+
 def timer (mat): #Recebe a matricula e calcula a soma
     global matricula
     n = len(mat)
@@ -31,12 +54,7 @@ def timer (mat): #Recebe a matricula e calcula a soma
     return time
     
 time = timer(mat)
-def getAngle(msg):
-    quaternion = msg.pose.pose.orientation
-    quat = [quaternion.x, quaternion.y, quaternion.z, quaternion.w]
-    euler = tf.transformations.euler_from_quaternion(quat)
-    yaw = euler[2]*180.0/math.pi
-    return yaw
+
 
 # CALLBACKS --------------------------------------------------------
 def odomCallBack(msg):
@@ -62,65 +80,74 @@ def timerCallBack(event):
     yaw = getAngle(odom)
     scan_len = len(scan.ranges)
 	
-
+    print ("Tamanho: ", scan_len)
+    print ("Angulo lido: ", yaw)
 	
-    if not (scan_len > 0):
+    if not(scan_len > 0):
         control2 = 0
         msg.linear.x = 0
 			
     elif state == 'state1':
-	      
-            if min(scan.ranges[scan_len-10 : scan_len+10]) < 100:
-           
-                msg.angular.z = 0
-                point = min (scan.ranges[scan_len-10 : scan_len+10])
-                setpoint2 = (200*((point - scan.ranges[0])/(scan.ranges[scan_len-1] - scan.ranges[0]))) - 100
-                error2 = (setpoint2 - yaw)
-                if abs(error2) > 180:
-                    if setpoint2 < 0:
-                        error2 += 360 
-                    else:
-                        error2 -= 360
-                P2 = kp2*error2
-                I2 = ki2*error2 + I2 
-                D2 = kd2*(error2 - erro2)
-                control2 = P2+I2+D2
-                erro2 = error2
-                msg.angular.z = control2
-                state = 'state2'   
+	
+    # POSICIONA DIRECAO ---------------------------------   
+		
+        print('Buscando...')
        
-                
-            else:	
-                if min(scan.ranges[scan_len-15 : scan_len+15]) < 100:
-                    msg.angular.z = 0.3*0.5
+        if min(scan.ranges[scan_len-10 : scan_len+10]) < 100:
+            print ("AAA")
+            msg.angular.z = 0
+            point = min (scan.ranges[scan_len-10 : scan_len+10])
+            setpoint2 = (200*((point - scan.ranges[0])/(scan.ranges[scan_len-1] - scan.ranges[0]))) - 100
+            print (setpoint2)
+            error2 = (setpoint2 - yaw)
+            if abs(error2) > 180:
+                if setpoint2 < 0:
+                    error2 += 360 
                 else:
-                    msg.angular.z = 0.3
-        
-
+                    error2 -= 360
+            P2 = kp2*error2
+            I2 = ki2*error2 + I2 #ki1*error1
+            D2 = kd2*(error2 - erro2)
+            control2 = P2+I2+D2
+            erro2 = error2
+            msg.angular.z = control2
+            state = 'state2'
+            
+             
+				
+        else:	
+            if min(scan.ranges[scan_len-15 : scan_len+15]) < 100:
+                print ("gira inferno")
+                msg.angular.z = 0.3*0.5
+            else:
+                msg.angular.z = 0.3
+           
+       
                     
     if state == 'state2':
-            setpoint3 = 0.5
+        setpoint3 = 0.5
     
-            scan_lenp = len(scan.ranges)
-            if scan_lenp > 0:
-                read = min(scan.ranges[scan_lenp-10 : scan_lenp+10])
+        scan_len = len(scan.ranges)
+        if scan_len > 0:
+            read = min(scan.ranges[scan_len-10 : scan_len+10])
 
-                error3 = -(setpoint3 - read)
+            error3 = -(setpoint3 - read)
         
-                P3 = kp3*error3
-                I3 = ki3*error3 + I3 
-                D3 = kd3*(error3 - erro3)
-                control3 = P3+I3+D3
-                erro3 = error3
+            P3 = kp3*error3
+            I3 = ki3*error3 + I3 #ki1*error1
+            D3 = kd3*(error3 - erro3)
+            control3 = P3+I3+D3
+            erro3 = error3
                 
-                if control3 > 1:
-                    control3 = 1
-                elif control3 < -1:
-                    control3 = -1
-            else:
-                control3 = 0        
+            if control3 > 1:
+                control3 = 1
+            elif control3 < -1:
+                control3 = -1
+        else:
+            control3 = 0        
         
-            msg.linear.x = control3
+        print (state)
+        msg.linear.x = control3
         
   
     pub.publish(msg)
@@ -130,6 +157,6 @@ pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 odom_sub = rospy.Subscriber('/odom', Odometry, odomCallBack)
 scan_sub = rospy.Subscriber('/scan', LaserScan, scanCallBack)
 
-timer = rospy.Timer(rospy.Duration(time), timerCallBack)
+timer = rospy.Timer(rospy.Duration(0.05), timerCallBack)
 
 rospy.spin()
